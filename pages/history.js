437aa81a8be6aeafd5a4abfec27e65d2481a655f
Angelina <angelina03wu@gmail.com>
2022-05-05 22:52:28 -0700

    made landing page

--- pages/history.js
+++ pages/history.js
@@ -4,13 +4,13 @@ import Styles from '../components/content.module.css'
 
 export default function Index() {
     return (
-    <Layout>
-    <Navbar />
-    <content className={Styles.content}>
-    <h2>Past History </h2>
-    <p> Rest of the website goes here </p>
-    </content>
-    </Layout>
+      <Layout>
+        <Navbar />
+        <content className={Styles.content}>
+        <h2>Past History </h2>
+        <p> Rest of the website goes here </p>
+        </content>
+      </Layout>
     )
   }
   
