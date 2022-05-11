05554b0b045484fee24adb20903cd5ef42d750b4
Joseph Nicolas <j2nicola@uwaterloo.ca>
2022-05-11 09:44:28 -0400

    Fixed delete button and added date section

--- components/PastInfo.js
+++ components/PastInfo.js
@@ -3,18 +3,20 @@ import Styles from './PastInfo.module.css'
 import { v4 as uuid } from 'uuid';
 
 export default function PastInfo() {
+    /*uuid is a temporary solution until we connect to the backend twitter API */
     const [hide, setHide] = useState({ display: 'none', });
     const unique_id1 = uuid();
     const unique_id2 = uuid();
-    const topic_id = unique_id1.slice(0,8)
-    const comment_id = unique_id2.slice(0,8)
-    const deleteBox = () => {
-        alert("Under Construction, will work soon");
-    }
+    const unique_id3 = uuid();
+    const unique_id4 = uuid();
+    const topic_id = unique_id1.slice(0,8);
+    const comment_id = unique_id2.slice(0,8);
+    const container_id = unique_id3.slice(0,8);
+    const date_id = unique_id4.slice(0,8)
     
     return (
         <hover>
-            <div className={Styles.container}
+            <div id={container_id} className={Styles.container}
                 onMouseEnter={() => {
                     setHide({ display: 'block' });
                 }}
@@ -23,14 +25,17 @@ export default function PastInfo() {
                 }}>
                 <div>
                     <div id={topic_id} className={Styles.topic}>
-                        Past Topic:
+                        Past Topic: 
                     </div>
                     <div id={comment_id} className={Styles.comment} contentEditable="false">
-                        Past Comment:
+                        Past Comment: 
+                    </div>
+                    <div id={date_id} className={Styles.date}>
+                        Date: 
                     </div>
                 </div>
                 <div>
-                    <button onClick={deleteBox} className={Styles.hoverButton} style={hide} >Delete</button>
+                    <button onClick={() => document.getElementById(container_id).remove()} className={Styles.hoverButton} style={hide} >Delete</button>
                     <button onClick={() => document.getElementById(comment_id).setAttribute("contenteditable", true)} className={Styles.hoverButton} style={hide}>Edit</button>
                 </div>
             </div>
