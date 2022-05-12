42308b9480422bff6bc92582e5ff27a8894af853
Joseph Nicolas <j2nicola@uwaterloo.ca>
2022-05-12 11:44:59 -0400

    Updated past info

--- components/PastInfo.js
+++ components/PastInfo.js
@@ -1,44 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Styles from './PastInfo.module.css'
-import { v4 as uuid } from 'uuid';
 
 export default function PastInfo() {
-    /*uuid is a temporary solution until we connect to the backend twitter API */
-    const [hide, setHide] = useState({ display: 'none', });
-    const unique_id1 = uuid();
-    const unique_id2 = uuid();
-    const unique_id3 = uuid();
-    const unique_id4 = uuid();
-    const topic_id = unique_id1.slice(0,8);
-    const comment_id = unique_id2.slice(0,8);
-    const container_id = unique_id3.slice(0,8);
-    const date_id = unique_id4.slice(0,8)
-    
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/comments?_limit=3`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((actualData) => {
+                setData(actualData);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setData(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <hover>
-            <div id={container_id} className={Styles.container}
-                onMouseEnter={() => {
-                    setHide({ display: 'block' });
-                }}
-                onMouseLeave={() => {
-                    setHide({ display: 'none' })
-                }}>
-                <div>
-                    <div id={topic_id} className={Styles.topic}>
-                        Past Topic: 
-                    </div>
-                    <div id={comment_id} className={Styles.comment} contentEditable="false">
-                        Past Comment: 
-                    </div>
-                    <div id={date_id} className={Styles.date}>
-                        Date: 
-                    </div>
-                </div>
-                <div>
-                    <button onClick={() => document.getElementById(container_id).remove()} className={Styles.hoverButton} style={hide} >Delete</button>
-                    <button onClick={() => document.getElementById(comment_id).setAttribute("contenteditable", true)} className={Styles.hoverButton} style={hide}>Edit</button>
-                </div>
-            </div>
+            {loading && <div>A moment please...</div>}
+            {error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            {data &&
+                data.map(({ id, body, name }) => (
+                    <a key={id}>
+                        <div id={id + "container"} className={Styles.container}
+                            onMouseEnter={() => {
+                                document.getElementById(id + "edit").style.display = 'block'
+                                document.getElementById(id + "delete").style.display = 'block'
+                            }}
+                            onMouseLeave={() => {
+                                document.getElementById(id + "edit").style.display = 'none'
+                                document.getElementById(id + "delete").style.display = 'none'
+                            }}>
+                            <div>
+                                <div className={Styles.topic}>
+                                    Past Topic: {name}
+                                </div>
+                                <div id={id + "comment"} className={Styles.comment} contentEditable="false">
+                                    Past Comment: {body}
+                                </div>
+                                <div id={id + "date"} className={Styles.date}>
+                                    Date:
+                                </div>
+                            </div>
+                            <div>
+                                <button id={id + "delete"} onClick={() => document.getElementById(id + "container").remove()} className={Styles.hoverButton}>Delete</button>
+                                <button id={id + "edit"} onClick={() => document.getElementById(id + "comment").setAttribute("contenteditable", true)} className={Styles.hoverButton}>Edit</button>
+                            </div>
+                        </div>
+                    </a>
+                ))}
         </hover>
     );
 }
