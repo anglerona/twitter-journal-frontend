39e336a7707910288e90c1c5a745bff72f607adb
Joseph Nicolas <j2nicola@uwaterloo.ca>
2022-05-12 18:28:18 -0400

    Refresh Button

---
+++ components/Refresh.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Styles from './Refresh.module.css'
+
+export default function Refresh() {
+  return (
+    <button className={Styles.refresh}>Refresh</button>
+  )
+}
--- pages/entry.js
+++ pages/entry.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Form from '../components/Form'
 import Tweets from '../components/Tweets'
+import Refresh from '../components/Refresh'
 
 
 export default function Index() {
@@ -9,6 +10,7 @@ export default function Index() {
     <Layout>
     <Navbar />
     <Tweets />
+    <Refresh />
     <Form />
     </Layout>
     )
